6c4c7e1dd12e84c2693e4409a08ff48336762684
Shoei Takamaru <shoei.takamaru@gmail.com>
2025-04-04 16:35:41 +0900

    ⚰️ Remove mason code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -81,29 +81,6 @@ require("CopilotChat").setup({
   debug = true,
 })
 
--- require("mason").setup()
-
---[[ local null_ls = require("null-ls")
-null_ls.setup({
-sources = {
-null_ls.builtins.formatting.prettier.with({
-prefer_local = "node_modules/.bin",
-}),
-on_attach = function(client, bufnr)
-if client.supports_method("textDocument/formatting") then
-vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-vim.api.nvim_create_autocmd("BufWritePre", {
-group = augroup,
-buffer = bufnr,
-callback = function()
-vim.lsp.buf.formatting_sync()
-end,
-})
-end
-end,
-}
-}) ]]
-
 -- ntpeters/vim-better-whitespace
 vim.g.better_whitespace_enabled = true
 vim.g.strip_whitespace_on_save = true
